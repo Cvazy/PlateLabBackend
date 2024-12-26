4c4931d530d2fa4dea8b0bf118b2f8c101de0a51
cvazy <player7712@mail.ru>
2024-12-26 06:46:50 +0300

    Внёс корректировки в модель соцсетей для поддержки SVG формата иконки

--- footer/admin.py
+++ footer/admin.py
@@ -1,5 +1,31 @@
 from django.contrib import admin
 from .models import *
 
+from django import forms
+from django.contrib import admin
+from .models import Network
+
+
+class NetworkAdminForm(forms.ModelForm):
+    class Meta:
+        model = Network
+        fields = '__all__'
+
+    def clean_image_icon(self):
+        image_icon = self.cleaned_data.get('image_icon')
+        if image_icon:
+            validate_image_or_vector(image_icon)
+        return image_icon
+
+
+@admin.register(Network)
+class NetworkAdmin(admin.ModelAdmin):
+    form = NetworkAdminForm
+
+    def formfield_for_dbfield(self, db_field, **kwargs):
+        if db_field.name == "image_icon":
+            kwargs["widget"] = forms.ClearableFileInput(attrs={"accept": "image/*"})
+        return super().formfield_for_dbfield(db_field, **kwargs)
+
+
 admin.site.register(Footer)
-admin.site.register(Network)
--- footer/models.py
+++ footer/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+import mimetypes
+from django.core.exceptions import ValidationError
 
 
 class Footer(models.Model):
@@ -12,10 +14,23 @@ class Footer(models.Model):
         return self.title
 
 
+def validate_image_or_vector(file):
+    mime_type, _ = mimetypes.guess_type(file.name)
+    allowed_mime_types = [
+        'image/jpeg', 'image/png', 'image/gif', 'image/svg+xml'
+    ]
+    if mime_type not in allowed_mime_types:
+        raise ValidationError(f'Unsupported file type: {mime_type}. Allowed types are JPEG, PNG, GIF, and SVG.')
+
+
 class Network(models.Model):
     name = models.CharField(max_length=255, help_text="Network name")
     link = models.CharField(max_length=255, help_text="Network link")
-    image_icon = models.ImageField(upload_to='images/networks', help_text="Network icon")
+    image_icon = models.FileField(
+        upload_to='images/networks',
+        help_text="Network icon",
+        validators=[validate_image_or_vector]
+    )
 
     class Meta:
         verbose_name = "Network"
